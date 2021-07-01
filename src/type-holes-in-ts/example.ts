c38d82c99f37b7f3c4c1873b0ee17b6f4f22ce5b
levin li <3259963@qq.com>
2021-07-01 11:28:42 +0800

    调整代码

--- src/type-holes-in-ts/example.ts
+++ src/type-holes-in-ts/example.ts
@@ -1,26 +1,100 @@
-// declare function jonk<A, B>(
-//   ab: (a: A) => B,
-//   ann: (an: (a: A) => number) => number
-// ): (bn: (b: B) => number) => number;
+//
+// Helper
+//
 
 declare function _<T>(): T;
 
-function jonk<A, B>(
-  ab: (a: A) => B,
-  ann: (an: (a: A) => number) => number
-): (bn: (b: B) => number) => number {
-  return (bn) => ann((a) => bn(ab(a)));
+//
+// Example 1
+//
+
+namespace S0 {
+  declare function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number;
 }
+
+namespace S1 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return _();
+  }
+}
+
+namespace S2 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return (bn) => _();
+  }
+}
+
+namespace S3 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return (bn) => ann(_());
+  }
+}
+
+namespace S4 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return (bn) => ann((a) => _());
+  }
+}
+
+namespace S5 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return (bn) => ann((a) => bn(_()));
+  }
+}
+
+namespace S6 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return (bn) => ann((a) => bn(ab(_())));
+  }
+}
+
+namespace S7 {
+  function jonk<A, B>(
+    ab: (a: A) => B,
+    ann: (an: (a: A) => number) => number
+  ): (bn: (b: B) => number) => number {
+    return (bn) => ann((a) => bn(ab(a)));
+  }
+}
+
+//
+// Example 2
+//
 
 import { foldLeft } from 'fp-ts/Array';
 import { pipe } from 'fp-ts/function';
 
-function zoop<A, B>(abb: (a: A) => (b: B) => B, b: B, as: Array<A>): B {
-  return pipe(
-    as,
-    foldLeft(
-      () => b,
-      (head, tail) => abb(head)(zoop(abb, b, tail))
-    )
-  );
+namespace E0 {
+  declare function zoop<A, B>(
+    abb: (a: A) => (b: B) => B,
+    b: B,
+    as: Array<A>
+  ): B;
+}
+
+namespace E0 {
+  function zoop<A, B>(abb: (a: A) => (b: B) => B, b: B, as: Array<A>): B {
+    return _();
+  }
 }
