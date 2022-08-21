1995afc469ade35daf46eca4d31cce0e9155c0a5
levin li <3259963@qq.com>
2022-08-21 10:18:16 +0800

    add code for comprehension of HKT

--- src/comprehension/HKT.ts
+++ src/comprehension/HKT.ts
@@ -1,1 +1,29 @@
 export const __N__ = 0;
+
+interface HKT_C<URI, A> {
+  readonly _URI: URI;
+  readonly _A: A;
+}
+
+interface URItoKind_C<A> {}
+
+type URIS_C = keyof URItoKind_C<any>;
+
+type Kind_C<URI extends URIS_C, A> = URI extends URIS_C ? URItoKind_C<A> : any;
+
+//
+// Usage
+//
+
+type RRR<A> = {
+  name: string;
+  body: A;
+};
+
+const URI = 'RRR';
+type URI = typeof URI;
+
+const x = <A>(a: A): HKT_C<URI, A> => ({
+  _URI: 'RRR',
+  _A: a,
+});
